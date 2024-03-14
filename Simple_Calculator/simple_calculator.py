bb50441064728e6c83dfbefda01518ad95380f07
Ali <syedmdali183@gmail.com>
2024-03-14 16:01:14 +0530

    first commit

---
+++ Simple_Calculator/simple_calculator.py
@@ -0,0 +1,53 @@
+def calculator():
+    print('Welcome to the simple calculator program')
+    print("Input first number")
+    number1= input()# prompt user to input a number
+    print("Choose an operation +,-,*,/")
+    a = input()# to input an operation choice
+    print("Input second number ")
+    number2= input()# prompt user to input a second number
+    # using try , except for handling errors when user inputs a wrong input like string or any complex numbers.
+    try:
+        if  a == "+":
+            print(float(number1)+float(number2))
+        elif a== "-":
+            print(float(number1)-float(number2))
+
+        elif a== "/":
+            print(float(number1)/float(number2))
+        elif a=="*":
+            print(float(number1)*float(number2))
+        else:
+            print("error, please choose a right operation")
+    except :
+        print('Input a valid number')
+if __name__ == "__main__":
+    while True:
+        calculator()
+        print('Want to perform another calcuation - yes/no')
+        choice = input().lower()
+        if choice == 'yes':
+            continue
+        elif choice == 'no':
+            print('Thank you')
+            break
+        else:
+            print('no right choice entered,Thank you for using ')
+            break
+
+
+
+
+    
+
+
+
+
+ 
+
+
+
+    
+
+
+
